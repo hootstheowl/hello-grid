dd6b2f3ca597f4ff23f795ebef44d6612e37687d
Shawn Scofield <hootstheowl@pm.me>
2022-10-24 23:31:03 -0500

    Updating formatting, refactoring util.js

--- src/util.js
+++ src/util.js
@@ -1,22 +1,22 @@
 import classNames from "classnames";
 
 /**
- * Generates classnames for margin, padding, or border
- * @param {string} borderType
+ * Generates classnames for center, margin, padding, and border
+ * @param {string} styleType
  * @param {*} value
  * @returns {Array}
  */
-function getClassNamesForBorderType(borderType, value) {
+const getClassNamesForStyleType = (styleType, value) => {
 	if (!!value === false) {
 		return [null];
 	}
 	if(Array.isArray(value) === true) {
 		if(value.length > 0 === true) {
-			return value.map(direction => `${borderType}-${direction}`);
+			return value.map(direction => `${styleType}-${direction}`);
 		}
 	}
 	if (typeof value === "string") {
-		return [`${borderType}-${value}`]
+		return [`${styleType}-${value}`]
 	}
 	return [null];
 }
@@ -26,47 +26,55 @@ function getClassNamesForBorderType(borderType, value) {
  * @param {string} componentClassName
  * @returns {Component}
  */
-export function buildGridComponent(componentClassName) {
-	const GridComponent = ({
-		className,
-		style,
-		centered,
-		padded,
-		margin,
-		bordered,
-		collapsible,
-		collapsed,
-		flex,
-        height,
-    	...props
-	}) => (
+export const buildGridComponent = (componentClassName) => (
+  ({
+    className,
+    style,
+    centered,
+    padded,
+    margin,
+    bordered,
+    collapsible,
+    collapsed,
+    flex,
+    height,
+    ...props
+  }) => (
 		<div
-			{...props}
+      {...props}
 			className={classNames(
-				componentClassName,
-                { bordered: !!bordered },
-                ...getClassNamesForBorderType("bordered", bordered),
-				{ padded: !!padded },
-				...getClassNamesForBorderType("padded", padded),
-				{ margin: !!margin },
-				...getClassNamesForBorderType("margin", margin),
-				{ centered: !!centered }, centered,
-				{ collapsible, collapsed },
-				className,
+        componentClassName,
+        {
+          bordered: !!bordered,
+          padded: !!padded,
+          margin: !!margin,
+          centered: !!centered,
+          collapsible: !!collapsible,
+          collapsed: !!collapsed,
+        },
+        getClassNamesForStyleType("centered", centered),
+        getClassNamesForStyleType("bordered", bordered),
+        getClassNamesForStyleType("padded", padded),
+        getClassNamesForStyleType("margin", margin),
+        className,
 			)}
-			style={{flex, height, maxHeight: height, ...style}}
+			style={{
+        flex,
+        height,
+        maxHeight: height,
+        ...style
+      }}
 		/>
-	);
-	return GridComponent;
-};
+	)
+);
 
 /**
  * Injects given stringified stylesheet into document head
  * @param {string} stylesheet
  * @returns {undefined}
  */
-export function injectStylesheet(stylesheet) {
-    const styleElement = document.createElement("style");
-    styleElement.textContent = stylesheet;
-    document.querySelector("head").appendChild(styleElement);
+export const injectStylesheet = (stylesheet) => {
+  const styleElement = document.createElement("style");
+  styleElement.textContent = stylesheet;
+  document.querySelector("head").appendChild(styleElement);
 };
